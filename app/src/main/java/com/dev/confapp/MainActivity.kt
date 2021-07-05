dac635583af1cfb438498ffa664c47469930c72e
azrougali <hm_azroug@esi.dz>
2021-07-05 21:04:34 +0100

    commit

--- app/src/main/java/com/dev/confapp/MainActivity.kt
+++ app/src/main/java/com/dev/confapp/MainActivity.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.react.modules.core.PermissionListener
+import org.jitsi.meet.sdk.JitsiMeetActivity
+import org.jitsi.meet.sdk.JitsiMeetActivityInterface
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions
+import org.jitsi.meet.sdk.JitsiMeetView
+import java.net.URL
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), JitsiMeetActivityInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,10 +21,16 @@ class MainActivity : AppCompatActivity() {
 
     fun EnterRoom(view : View){
 
-        val token = findViewById<EditText>(R.id.token).toString()
+        val view = JitsiMeetView(this)
+        val options = JitsiMeetConferenceOptions.Builder()
+            .setServerURL(URL("https://meet.jit.si/"))
+            .setRoom("222222")
+            .setAudioMuted(true)
+            .build()
+        JitsiMeetActivity.launch(this, options)
+    }
 
-        val intent = Intent(this,ChooseRoom::class.java)
-        intent.putExtra("token",token)
-        startActivity(intent)
+    override fun requestPermissions(p0: Array<out String>?, p1: Int, p2: PermissionListener?) {
+        TODO("Not yet implemented")
     }
 }
